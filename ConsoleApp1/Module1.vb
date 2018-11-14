636577c9dd3b015b77ecea98e193e92ad41dbe83
Parul Dixit <parul.dixit@synapse.com>
2018-11-14 11:01:47 +0530

    first commit

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello this this demo project")
+        Console.Write("**************************")
+
+
+        Console.Write("Press any key to continue...")
+        Console.ReadKey(True)
+    End Sub
+
+End Module
